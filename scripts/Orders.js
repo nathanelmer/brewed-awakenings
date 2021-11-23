48941f7293681d62a3df101a1d7acb0c98c1b690
nathanelmer <nelmer12@gmail.com>
2021-11-23 14:03:13 -0600

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,54 @@
+import { getProducts, getEmployees, getOrders } from "./database.js";
+
+// Get copy of state for use in this module
+const products = getProducts();
+const employees = getEmployees();
+const orders = getOrders();
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+  let orderProduct = null;
+
+  for (const product of allProducts) {
+    if (product.id === order.productId) {
+      orderProduct = product;
+    }
+  }
+
+  return orderProduct;
+};
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+  let orderEmployee = null;
+
+  for (const employee of allEmployees) {
+    if (employee.id === order.employeeId) {
+      orderEmployee = employee;
+    }
+  }
+
+  if (orderEmployee === null) {
+    console.log("employee id not found");
+  } 
+  
+  return orderEmployee;
+};
+
+export const Orders = () => {
+  let html = "<ul>";
+
+  for (const order of orders) {
+    const employee = findEmployee(order, employees);
+    const product = findProduct(order, products);
+    const employeeName = (employee === null) ? 'Not Found' : employee.name;
+
+    html += `<li>${(product === null) ? 'Not Found' : product.name} was sold by ${employeeName} on ${new Date(
+      order.timestamp
+    ).toLocaleDateString()}</li>`;
+  }
+
+  html += "</ul>";
+
+  return html;
+};
